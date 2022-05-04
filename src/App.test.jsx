c79efd593af214216672abfd09c93c3e889051f8
Pete Seeling <peteseeling@gmail.com>
2022-05-04 09:55:44 -0600

    started writing app.test.js

---
+++ src/App.test.jsx
@@ -0,0 +1,14 @@
+import {
+    render, screen, waitForElementToBeRemoved
+} from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import App from './App';
+
+describe('<App />', () => {
+    it('renders clickable names on list view. Onclick redirects to detail page', async () => {
+        render(
+            <MemoryRouter 
+        )
+    })
+})
